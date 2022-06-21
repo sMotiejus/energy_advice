f222015310af18f352eed3cc6298c2740c89d462
sMotiejus <motiejusslim@gmail.com>
2022-06-21 20:13:54 +0300

    prijungta svetaine prie EXPRESS

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
+function ShowFavicon(end){
+    let start= "https://s2.googleusercontent.com/s2/favicons?domain=";
+    return start+end;
+}
 
 function App() {
 
@@ -16,17 +21,37 @@ function App() {
     }, [])
 
     return (
-        <div>
-            
-            {(typeof urls.users === 'undefined') ? (
-                <p>Loading...</p>
-            ):
-            (
-                urls.users.map((user, i) => (
-                    <p key={i}>{user}</p>
-                ))
-            )}
-
+        <div className="App">
+            <header className='App-header'>
+                Most Popular Lithuanian Sites
+            </header>
+            <div className="App-Body"> 
+                <div className="grid-container">
+                    {(typeof urls.urlsList === 'undefined')? 
+                    (
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status">
+                                <span class="sr-only"></span>
+                            </div>
+                        </div>
+                    )
+                    :
+                    (
+                        urls.urlsList.map(url =>
+                            <div key={url.id} className="webComponent">
+                                <div className="grid-component-on">
+                                    <div className="img"><img src={ShowFavicon(url.url)} alt="Favicon" /></div>
+                                    {url.title}
+                                </div>
+                                <div key={url.id} className="grid-component-off">
+                                    <img src={process.env.PUBLIC_URL + "/SS/"+url.title.replace(/\s/g, '')+".png"} alt="SS"/>
+                                    <a href={url.url}>{url.url}</a>
+                                </div>
+                            </div>         
+                        )
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const json = require('./data/db.json');
 
 app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"] })
+    res.json(json);
 })
 
 app.listen(port, () =>{console.log(`${port}`)})
